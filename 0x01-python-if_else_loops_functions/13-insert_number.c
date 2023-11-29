d8ce8e3de396be37b96244f16ad419081e87fbd4
Abdullah-Sufyan <hanesYL14@gmail.com>
2023-11-28 17:50:35 -0800

    0x01-python-if_else_loops_functions task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,74 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted singly linked list.
+ * @head: Pointer to the head of the list.
+ * @number: Number to be inserted.
+ *
+ * Return: Address of the new node, or NULL if it failed.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+	new_node->n = number;
+	new_node->next = NULL;
+
+	listint_t *current = *head;
+	listint_t *prev = NULL;
+
+	if (!current || current->n >= number)
+	{
+		new_node->next = current;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (current && current->n < number)
+	{
+		prev = current;
+		current = current->next;
+	}
+
+	prev->next = new_node;
+	new_node->next = current;
+
+	return (new_node);
+}
+
+/**
+ * print_list - Prints all elements of a linked list.
+ * @head: Pointer to the head of the list.
+ */
+void print_list(listint_t *head)
+{
+	while (head != NULL)
+	{
+		printf("%d ", head->n);
+		head = head->next;
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	listint_t *head = NULL;
+
+	insert_node(&head, 5);
+	insert_node(&head, 10);
+	insert_node(&head, 2);
+	insert_node(&head, 7);
+	printf("Linked list after insertion: ");
+	print_list(head);
+
+	return (0);
+}
