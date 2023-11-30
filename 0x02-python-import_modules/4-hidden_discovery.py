44f337af92b40607f6f4a74b36e10bddefe74261
Abdullah-Sufyan <hanesYL14@gmail.com>
2023-11-30 08:46:39 -0800

    0x02-python-import_modules task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import importlib.util
 
+
 def print_names():
-    spec = importlib.util.spec_from_file_location("hidden_4", "C:/Users/HP/Downloads/hidden_.pyc")
+    spec = importlib.util.spec_from_file_location
+    ("hidden_4", "C:/Users/HP/Downloads/hidden_.pyc")
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
-    
+
     for name in dir(module):
         if not name.startswith('__'):
             print(name)
+
 
 if __name__ == "__main__":
     print_names()
